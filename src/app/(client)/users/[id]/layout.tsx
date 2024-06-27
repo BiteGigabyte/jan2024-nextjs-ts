e1e551c67db663639f23758125358d9a4bb2612f
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-06-27 23:50:45 +0300

    переписав урок 1 (маршрутизація і запити)

--- src/app/(client)/users/[id]/layout.tsx
+++ src/app/(client)/users/[id]/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {Metadata} from "next";
 
+
 export const generateMetadata = async ({params}: {params: {id: string}}): Promise<Metadata> => {
     let user: IUser = await fetch('https://jsonplaceholder.typicode.com/users/' + params.id)
         .then(value => value.json());
-
 
     return {
         title: user.email,
@@ -16,9 +16,9 @@ type Props = { children: React.ReactNode }
 
 const UserLayout = ({children} : Props) => {
     return (
-        <>
+        <div>
         {children}
-        </>
+        </div>
     );
 };
 
