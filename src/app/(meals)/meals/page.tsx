645f5096a7cd0edc04f5d953aa986db9b6f1b1c5
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-07-01 21:46:15 +0300

    переписую 2 урок дз
    трошки пописали зробив витяг з бази даних інформації файли які в корні їх можна б було не брати до уваги вони заміняють базу даних
    
    записуємо в базу даних 31:00

--- src/app/(meals)/meals/page.tsx
+++ src/app/(meals)/meals/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {getMeals} from "@/services/db/db";
+import FormComponent from "@/components/form-component/FormComponent";
 
 const MealsPage = async () => {
 
@@ -8,6 +9,7 @@ const MealsPage = async () => {
 
     return (
         <div>
+            <FormComponent/>
             {meals.map(meal => <div key={meal.id}>{meal.title}</div>)}
         </div>
     );
--- src/services/db/db.ts
+++ src/services/db/db.ts
@@ -1,4 +1,8 @@
+'use server' //top level server directive (всі функції являються серверними)
+
+
 import sql from 'better-sqlite3';
+import {revalidatePath} from "next/cache";
 
 let db = sql('meals.db');
 
@@ -6,6 +10,26 @@ const getMeals = async (): Promise<IMeal[]> => {
     return db.prepare<IMeal[]>('select * from meals').all() as IMeal[];
 }
 
+const addMeal = async (formData: FormData): Promise<void> => {
+    // 'use server' //line level server directive
+
+    db
+        .prepare('insert into meals(slug, title, image, summary, instructions, creator, creator_email) values (?,?,?,?,?,?,?)')
+        .run(
+            formData.get('slug'),
+            formData.get('title'),
+            formData.get('image'),
+            formData.get('summary'),
+            formData.get('instructions'),
+            formData.get('creator'),
+            formData.get('creator_email')
+        );
+        // revalidatePath('/meals', 'layout');   //2 аргумент це на якому рівні рендерити (автоматично рендерить на рівні на якому знаходитесь)
+        // revalidatePath('/meals', 'page');     //2 аргумент це на якому рівні рендерити (автоматично рендерить на рівні на якому знаходитесь)
+        revalidatePath('/meals');
+}
+
 export {
-    getMeals
+    getMeals,
+    addMeal
 }
